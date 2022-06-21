86ddd445bf6afec0eef021cd19dbb197a2d21d5b
Alejandro <alixxantada@gmail.com>
2022-06-21 16:50:09 +0200

    feature: Calculo tiempo ejecucion ok, bugueado captura de errores

---
+++ src/7.1-child-practice/child.js
@@ -0,0 +1,47 @@
+console.log("child started working");
+
+process.on("message", (arg) => {
+  parOImpar(arg.seed);
+});
+
+function parOImpar(seed) {
+  let seedOriginal = " " + seed;
+  let seedTexto = "" + seed;
+  let seedOpera = seed;
+  const start = process.hrtime();
+
+  try {
+    if (!parseInt(seed)) {
+      throw new Error("No es un número válido ");
+    }
+    if (seedOpera <= 0) {
+      seedTexto = "error";
+    }
+
+    while (seedOpera > 1) {
+      if (isEven(seedOpera)) {
+        seedOpera = seedOpera / 2;
+        seedTexto += " " + seedOpera;
+      } else {
+        seedOpera = seedOpera * 3 + 1;
+        seedTexto += " " + seedOpera;
+      }
+    }
+    const end = process.hrtime();
+    const tiempoEjecucion = end[1] - start[1];
+    seedTexto += " , Nanosecond Start: " + start[1];
+    seedTexto += " , Nanoseconds End: " + end[1];
+    seedTexto += " , Total Nanoseconds : " + tiempoEjecucion;
+
+    process.send({ semillas: seedTexto, seedOriginal: seedOriginal });
+    process.exit(0);
+  } catch (error) {
+    console.log( error + ", Error Fatal!");    
+    process.send({ msg: "Child Error", err: error, seed: seed });    
+    process.exit(1);
+  }
+}
+
+function isEven(seed) {
+  return seed % 2 == 0;
+}
---
+++ src/7.1-child-practice/index.js
@@ -0,0 +1,28 @@
+import * as cp from "child_process";
+import * as fs from "fs/promises";
+
+const seeds = [0, 1, 2, 3, 5, 7, 11, 13, 'wsdfs'];
+let child;
+const scriptFile = process.argv[1];
+
+seeds.forEach((seed) => {
+    child = cp.fork("./src/7.1-child-practice/child.js");
+    child.send({ seed });
+    child.on("message", (arg) => {
+        writeFile(arg.semillas, arg.seedOriginal, arg.err);        
+        console.log(`Recibido error: ${arg.msg} de ${arg.seed} con error: ${arg.err}`);        
+    });
+});
+
+
+
+async function writeFile(semillas, seedOriginal) {
+  await fs.writeFile(`${scriptFile}.${seedOriginal}.txt`, semillas)
+    .then(onWriteEnd)
+    .catch((err) => console.log(`${err}`))
+    .finally(() => console.log("Finalizado"));
+};
+
+function onWriteEnd() {
+  console.log(`Resultado OK`);
+}
