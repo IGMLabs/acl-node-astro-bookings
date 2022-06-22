7ab549cece42604485a073492b38e42d43789854
Alejandro <alixxantada@gmail.com>
2022-06-22 11:07:41 +0200

    feature: refactorizado child practice, codigo limpio

--- src/7.1-child-practice/child.js
+++ src/7.1-child-practice/child.js
@@ -1,45 +1,75 @@
 console.log("child started working");
-
-process.on("message", (arg) => {
-  parOImpar(arg.seed);
+process.on("message", (message) => {
+  const startTime = process.hrtime();
+  try {
+    const secuencia = generarSecuencia(message.seed);
+    sendResponse(startTime, secuencia, null);
+  } catch (error) {
+    console.log("Child error! ", error);
+    sendResponse(startTime, "", error);
+  }
 });
 
-function parOImpar(seed) {
-  let seedOriginal = " " + seed;
-  let seedTexto = "" + seed;
-  let seedOpera = seed;
-  const start = process.hrtime();
-
-  try {
-    if (!parseInt(seed)) {
-      throw new Error("No es un número válido ");
-    }
-    if (seedOpera <= 0) {
-      seedTexto = "error";
-    }
-
-    while (seedOpera > 1) {
-      if (isEven(seedOpera)) {
-        seedOpera = seedOpera / 2;
-        seedTexto += " " + seedOpera;
-      } else {
-        seedOpera = seedOpera * 3 + 1;
-        seedTexto += " " + seedOpera;
-      }
-    }
-    const end = process.hrtime();
-    const tiempoEjecucion = end[1] - start[1];
-    seedTexto += " , Nanosecond Start: " + start[1];
-    seedTexto += " , Nanoseconds End: " + end[1];
-    seedTexto += " , Total Nanoseconds : " + tiempoEjecucion;
-
-    process.send({ semillas: seedTexto, seedOriginal: seedOriginal });
-    process.exit(0);
-  } catch (error) {
-    console.log( error + ", Error Fatal!");    
-    process.send({ msg: "Child Error", err: error, seed: seed });    
+function sendResponse(startTime, secuencia, error) {
+  const endTime = process.hrtime();
+  const firma = firmarSecuencia(startTime, endTime);
+  const secuenciaFirmada = secuencia + " " + firma;
+  if (error) {
+    const errorMessage = buildErrorMessage(error, firma);
+    process.send(errorMessage);
     process.exit(1);
+  } else {
+    const okMessage = buildOkMessage(secuenciaFirmada);
+    process.send(okMessage);
+    process.exit(0);
   }
+}
+
+function generarSecuencia(seed) {
+  if (!parseInt(seed)) {
+    throw new Error(`No es un número válido ${seed}`);
+  }
+  if (seed <= 0) {
+    return "";
+  }
+  if (seed === 1) {
+    return "1";
+  }
+  let secuencia = "" + seed;
+  let valorActual = seed;
+
+  while (valorActual > 1) {
+    valorActual = generateNewValue(valorActual);
+    secuencia += " " + valorActual;
+  }
+  return secuencia;
+}
+
+function generateNewValue(valorActual) {
+  let newValue = 0;
+  if (isEven(valorActual)) {
+    newValue = valorActual / 2;
+  } else {
+    newValue = valorActual * 3 + 1;
+  }
+  return newValue;
+}
+
+function firmarSecuencia(startTime, endTime) {
+  let firma = "";
+  const tiempoEjecucion = endTime[1] - startTime[1];
+  firma += " , Nanosecond Start: " + startTime[1];
+  firma += " , Nanoseconds End: " + endTime[1];
+  firma += " , Total Nanoseconds : " + tiempoEjecucion;
+  return firma;
+}
+
+function buildErrorMessage(err, secuencia) {
+  return { err: err.message, secuencia: secuencia };
+}
+
+function buildOkMessage(secuencia) {
+  return { secuencia: secuencia };
 }
 
 function isEven(seed) {
